d4c0789af8d61fb5dec8e25b56251905ca271027
arkablok <ihpazlagasukanda2403@gmail.com>
2025-05-15 15:09:03 +0700

    .

--- routes/triggerCron.js
+++ routes/triggerCron.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require("../config/db"); // Sesuaikan dengan koneksi MySQL kamu
 const { generateAllReports } = require('../services/reportGenerator');
 
-router.get('/generate-excel-2403', async (req, res) => {
+router.get('/generate-excel', async (req, res) => {
     generateAllReports();
     res.send('Excel file generated manually.');
 });
